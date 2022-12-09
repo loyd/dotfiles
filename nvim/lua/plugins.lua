83a545a4c545c71e0aa75adc1d4f82e2eb1ca1fd
Paul Loyd <pavelko95@gmail.com>
2022-12-09 15:26:37 +0800

    feat(nvim/crates): move to v0.3.0 & enable code actions

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,6 +3,9 @@ local use = packer.use
 
 packer.init()
 
+---- Common deps
+use("nvim-lua/plenary.nvim")
+
 ---- Interface
 use("nvim-lualine/lualine.nvim")
 use({ "gruvbox-material/vim", as = "gruvbox-material" })
@@ -20,7 +23,7 @@ use("terryma/vim-multiple-cursors")
 use("LunarWatcher/auto-pairs")
 use("machakann/vim-highlightedyank")
 use("rrethy/vim-illuminate")
-use("hrsh7th/vim-vsnip")
+use("hrsh7th/vim-vsnip") -- TODO: remove snippets at all
 use("ggandor/leap.nvim")
 
 ---- Integrations
@@ -31,28 +34,27 @@ use("neovim/nvim-lspconfig")
 use("simrat39/rust-tools.nvim")
 use("mfussenegger/nvim-dap")
 use("rcarriga/nvim-dap-ui")
+use({ "saecki/crates.nvim", tag = "v0.3.0" })
 use("hrsh7th/nvim-cmp")
 use("hrsh7th/cmp-nvim-lsp")
 use("hrsh7th/cmp-path")
 use("hrsh7th/cmp-buffer")
 use("hrsh7th/cmp-cmdline")
 use("hrsh7th/cmp-vsnip")
-use({ "saecki/crates.nvim", tag = "v0.2.1" })
 use("editorconfig/editorconfig-vim")
 use("sindrets/diffview.nvim")
 use("j-hui/fidget.nvim")
 use("SmiteshP/nvim-navic")
 use("jose-elias-alvarez/null-ls.nvim")
 
----- Misc
-use("nvim-lua/plenary.nvim")
-use("natecraddock/sessions.nvim")
-
 ---- Languages
 use("sheerun/vim-polyglot")
 use("hynek/vim-python-pep8-indent")
 use("pest-parser/pest.vim")
 use("ron-rs/ron.vim")
+
+---- Misc
+use("natecraddock/sessions.nvim")
 
 ---- Settings
 require("settings.treesitter")
