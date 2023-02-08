fb3098cdca1a13f44dea6592debf9c59141754ef
Paul Loyd <pavelko95@gmail.com>
2023-02-08 14:11:59 +0800

    fix(nvim/project): use only patterns

--- nvim/lua/settings/project.lua
+++ nvim/lua/settings/project.lua
@@ -1,6 +1,8 @@
 local nmap = require("helpers.map").nmap
 
-require("project_nvim").setup({})
+require("project_nvim").setup({
+    detection_methods = { "pattern" },
+})
 
 require("telescope").load_extension("projects")
 nmap("<leader>p", ":Telescope projects<CR>")
