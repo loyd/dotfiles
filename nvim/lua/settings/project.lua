b4c374f28981d9dc20f0498885bc77de304ceb20
Paul Loyd <pavelko95@gmail.com>
2023-07-09 12:07:29 +0400

    feat(nvim/project): ignore .cargo and .rustup

--- nvim/lua/settings/project.lua
+++ nvim/lua/settings/project.lua
@@ -2,6 +2,7 @@ local nmap = require("helpers.map").nmap
 
 require("project_nvim").setup({
     detection_methods = { "pattern" },
+    exclude_dirs = { "~/.cargo/*", "~/.rustup/*" },
 })
 
 require("telescope").load_extension("projects")
