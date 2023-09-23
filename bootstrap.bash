859e173c8466e7dd9e9ca7430af69980fe72a105
Paul Loyd <pavelko95@gmail.com>
2023-09-23 20:40:50 +0400

    feat: add bootstrap.bash

---
+++ bootstrap.bash
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+ln -s /home/workspace/fave/dotfiles/nvim ~/.config
+ln -s /home/workspace/fave/dotfiles/alacritty ~/.config
+ln -s /home/workspace/fave/dotfiles/kitty ~/.config
+# TODO: i3, polybar, picom, rofi, dunst, etc.
