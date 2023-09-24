524e50708087b760b0655d6ec083b99395cd5d70
Paul Loyd <pavelko95@gmail.com>
2023-09-24 16:24:20 +0400

    fix(nvim/diffview): jump to prev tab on close

--- nvim/lua/settings/diffview.lua
+++ nvim/lua/settings/diffview.lua
@@ -1,10 +1,10 @@
 local nmap = require("helpers.map").nmap
 
--- TODO: jump to prev tab when closed.
 -- TODO: make colors close to nvim-tree.
 
 nmap("<leader>go", ":DiffviewOpen<CR>", "Diffview: compare against the index")
 nmap("<leader>gM", ":DiffviewOpen master<CR>", "Diffview: compare against the master")
-nmap("<leader>gq", ":DiffviewClose<CR>", "Diffview: close the active view")
+-- https://github.com/sindrets/diffview.nvim/issues/316
+nmap("<leader>gq", ":tabn # | tabc #<CR>", "Diffview: close the active view")
 nmap("<leader>gh", ":DiffviewFileHistory %<CR>", "Diffview: show history of the current file")
 nmap("<leader>gH", ":DiffviewFileHistory<CR>", "Diffview: show history of the working tree")
