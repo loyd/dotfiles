2097073a7da4645d068fd76d2313e076bf29778c
Paul Loyd <pavelko95@gmail.com>
2024-05-25 13:30:16 +0400

    feat(nvim/diffview): unify bg with nvim-tree

--- nvim/lua/settings/diffview.lua
+++ nvim/lua/settings/diffview.lua
@@ -1,6 +1,5 @@
 local nmap = require("helpers.map").nmap
-
--- TODO: make colors close to nvim-tree.
+local augroup = require("helpers.augroup")
 
 nmap("<leader>go", ":DiffviewOpen<CR>", "Diffview: compare against the index")
 nmap("<leader>gM", ":DiffviewOpen master<CR>", "Diffview: compare against the master")
@@ -8,3 +7,13 @@ nmap("<leader>gM", ":DiffviewOpen master<CR>", "Diffview: compare against the ma
 nmap("<leader>gq", ":tabn # | tabc #<CR>", "Diffview: close the active view")
 nmap("<leader>gh", ":DiffviewFileHistory %<CR>", "Diffview: show history of the current file")
 nmap("<leader>gH", ":DiffviewFileHistory<CR>", "Diffview: show history of the working tree")
+
+-- Link `DiffviewNormal` to `NvimTreeNormal` to unify background colors.
+augroup("DiffviewPanelBg", function(autocmd)
+    local function fix_bg()
+        vim.api.nvim_set_hl(0, "DiffviewNormal", { link = "NvimTreeNormal" })
+    end
+    fix_bg()
+
+    autocmd("ColorScheme", {}, fix_bg)
+end)
