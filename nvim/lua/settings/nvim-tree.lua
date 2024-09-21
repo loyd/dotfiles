66c4e6bd02c133e908e529827cae39e434ae3d64
Paul Loyd <pavelko95@gmail.com>
2024-09-21 23:05:31 +0400

    fix(nvim/nvim-tree): remove autocmd for auto-start in new tabs

--- nvim/lua/settings/nvim-tree.lua
+++ nvim/lua/settings/nvim-tree.lua
@@ -1,8 +1,5 @@
-local thunk = require("helpers.func").thunk
 local nmap = require("helpers.map").nmap
-local augroup = require("helpers.augroup")
 local symbols = require("settings._symbols")
-local nt_api = require("nvim-tree.api")
 
 -- TODO: diagnostics disappear when switching between tabs.
 -- TODO: start on the first file after startup.
@@ -44,11 +41,6 @@ require("nvim-tree").setup({
         custom = { "^.git$" },
     },
 })
-
--- Start nvim-tree in new tabs.
-augroup("NvimTreeNewTab", function(autocmd)
-    autocmd("TabNewEntered", {}, thunk(nt_api.tree.open))
-end)
 
 nmap("<leader>tt", ":NvimTreeToggle<CR>", "Toggle the file tree")
 nmap("<leader>F", ":NvimTreeFocus<CR>", "Focus the file tree")
