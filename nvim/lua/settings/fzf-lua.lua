34f31984d5b27e5f22b7323f471481076c31c4a7
Paul Loyd <pavelko95@gmail.com>
2024-09-24 11:01:16 +0400

    feat(nvim/fzf-lua): revise keybindings

--- nvim/lua/settings/fzf-lua.lua
+++ nvim/lua/settings/fzf-lua.lua
@@ -1,17 +1,12 @@
 local fzf = require("fzf-lua")
+local actions = require("fzf-lua.actions")
 local nmap = require("helpers.map").nmap
 local vmap = require("helpers.map").vmap
 local partial = require("helpers.func").partial
 
 fzf.setup({
-    "skim",
     winopts = {
         backdrop = 80,
-    },
-    files = {
-        prompt = "Files> ",
-        cwd_prompt = false,
-        formatter = "path.dirname_first",
     },
     previewers = {
         ["custom:project"] = {
@@ -22,6 +17,68 @@ fzf.setup({
             }; s]],
             _ctor = require("fzf-lua.previewer").fzf.cmd,
         },
+    },
+    keymap = {
+        builtin = {
+            false,
+            ["<F1>"] = "toggle-help",
+            ["<F2>"] = "toggle-fullscreen",
+            ["<F3>"] = "toggle-preview-wrap",
+            ["<F4>"] = "toggle-preview",
+            ["<F5>"] = "toggle-preview-ccw",
+            ["<F6>"] = "toggle-preview-cw",
+            ["<C-d>"] = "preview-page-down",
+            ["<C-u>"] = "preview-page-up",
+        },
+        fzf = {
+            false,
+            ["f3"] = "toggle-preview-wrap",
+            ["f4"] = "toggle-preview",
+            ["ctrl-d"] = "preview-page-down",
+            ["ctrl-u"] = "preview-page-up",
+            ["ctrl-a"] = "beginning-of-line",
+            ["ctrl-e"] = "end-of-line",
+            ["alt-n"] = "half-page-down",
+            ["alt-p"] = "half-page-up",
+            ["alt-g"] = "last",
+            ["alt-h"] = "first",
+            ["tab"] = "toggle",
+            ["shift-tab"] = "toggle-all",
+        },
+    },
+    actions = {
+        files = {
+            false,
+            ["ctrl-q"] = false,
+            ["enter"] = actions.file_edit_or_qf,
+            ["ctrl-x"] = actions.file_split,
+            ["ctrl-v"] = actions.file_vsplit,
+            ["ctrl-t"] = actions.file_tabedit,
+        },
+    },
+
+    -- Pickers
+    files = {
+        prompt = "Files> ",
+        cwd_prompt = false,
+        formatter = "path.dirname_first",
+        actions = {
+            ["ctrl-z"] = actions.toggle_hidden,
+        },
+    },
+    git = {
+        status = {
+            actions = {
+                ["right"] = false,
+                ["left"] = false,
+                ["ctrl-x"] = { fn = actions.git_reset, reload = true },
+                ["ctrl-g"] = { fn = actions.git_stage_unstage, reload = true },
+            },
+        },
+    },
+    keymaps = {
+        previewer = false,
+        show_details = false,
     },
 })
 
@@ -56,12 +113,14 @@ nmap("<leader>lb", fzf.git_branches, "List branches for the current project")
 nmap("<leader>lg", fzf.git_status, "List git status for the current project")
 
 nmap("<leader>la", fzf.lsp_code_actions, "List code actions for the item under the cursor")
-nmap("<leader>lr", fzf.lsp_references, "List references for the symbol under the cursor")
 nmap("<leader>ls", fzf.lsp_document_symbols, "List symbols for the current buffer")
 nmap("<leader>lS", fzf.lsp_workspace_symbols, "List symbols for the current project")
-nmap("<leader>li", fzf.lsp_implementations, "List implementations for the symbol under the cursor")
-nmap("<leader>ld", fzf.lsp_definitions, "List definitions for the symbol under the cursor")
-nmap("<leader>lt", fzf.lsp_typedefs, "List type definitions for the symbol under the cursor")
+
+local opts = { jump_to_single_result = true }
+nmap("<leader>lt", partial(fzf.lsp_typedefs, opts), "List type definitions for the symbol under the cursor")
+nmap("<leader>ld", partial(fzf.lsp_definitions, opts), "List definitions for the symbol under the cursor")
+nmap("<leader>lr", partial(fzf.lsp_references, opts), "List references for the symbol under the cursor")
+nmap("<leader>li", partial(fzf.lsp_implementations, opts), "List implementations for the symbol under the cursor")
 
 nmap(
     "<leader>P",
@@ -70,6 +129,7 @@ nmap(
         cmd = "fd --exact-depth 2 -t d",
         cwd = "~/workspace",
         previewer = "custom:project",
+        header = false,
     }),
     "List projects"
 )
