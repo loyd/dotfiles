ad9cc3e22dc6f35eeb7b01392676c3f81365bd95
Paul Loyd <pavelko95@gmail.com>
2024-10-13 13:47:50 +0400

    feat(nvim/nvim-tree): enable "update_root"

--- nvim/lua/settings/nvim-tree.lua
+++ nvim/lua/settings/nvim-tree.lua
@@ -6,7 +6,10 @@ local symbols = require("settings._symbols")
 
 require("nvim-tree").setup({
     sync_root_with_cwd = true,
-    update_focused_file = { enable = true },
+    update_focused_file = {
+        enable = true,
+        update_root = { enable = true },
+    },
     view = { width = 35 }, -- diffview style
     renderer = {
         root_folder_label = false,
