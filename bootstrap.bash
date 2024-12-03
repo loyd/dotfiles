bd2dacc0d3d74c14b445e37b566646ca675f9fdf
Paul Loyd <pavelko95@gmail.com>
2024-12-03 13:49:29 +0400

    feat: update bootstrap

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,6 +1,11 @@
 #!/bin/env bash
 
-ln -s /home/workspace/fave/dotfiles/nvim ~/.config
-ln -s /home/workspace/fave/dotfiles/alacritty ~/.config
-ln -s /home/workspace/fave/dotfiles/kitty ~/.config
-# TODO: i3, polybar, picom, rofi, dunst, etc.
+CFG=/home/code/fave/dotfiles
+
+ln -s $CFG/nvim ~/.config
+ln -s $CFG/alacritty ~/.config
+ln -s $CFG/kitty ~/.config
+ln -s $CFG/niri ~/.config
+ln -s $CFG/waybar ~/.config
+ln -s $CFG/fuzzel ~/.config
+ln -s $CFG/mako ~/.config
