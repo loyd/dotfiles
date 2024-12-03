01141bc98a01fb8b9a79501312d44bcf278678fc
Paul Loyd <pavelko95@gmail.com>
2024-12-03 13:46:32 +0400

    feat(fuzzel): initial config

---
+++ fuzzel/powermenu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SELECTION="$(printf "[1] Lock\n[2] Suspend\n[3] Log out\n[4] Reboot\n[5] Reboot to UEFI\n[6] Shutdown" | fuzzel --dmenu -l 6 -p "Power Menu: ")"
+
+case $SELECTION in
+    *"Lock")
+        swaylock;;
+    *"Suspend")
+        systemctl suspend;;
+    *"Log out")
+        niri msg action quit -s;;
+    *"Reboot")
+        systemctl reboot;;
+    *"Reboot to UEFI")
+        systemctl reboot --firmware-setup;;
+    *"Shutdown")
+        systemctl poweroff;;
+esac
